229e4c273906a9deadf00bcc32632afd2437614f
Marco De Canal <marco.decanal@gmail.com>
2020-06-10 11:09:33 +0200

    Scheletro html sistemato

--- gestione/views.py
+++ gestione/views.py
@@ -6,15 +6,19 @@ from .models import Paziente, Fattura
 # Create your views here.
 
 class PazienteListView(generic.ListView):
+    """View di elenco dei pazienti, home page del sito"""
     model = Paziente
 
 class PazienteDetailView(generic.DetailView):
+    """Vista dettagliata del paziente, per modifica e generazione di fattura"""
     model = Paziente
 
 class FatturaListView(generic.ListView):
+    """Vista di elenco delle fatture, per la stampa o l'eliminazione"""
     model = Fattura
 
-class FatturaDetailView(generic.ListView):
+class FatturaDetailView(generic.DetailView):
+    """Vista di dettaglio della fattura, ovvero il file da stampare"""
     model = Fattura
 
 #da definire, la View per l'inserimento di un nuovo paziente
