7dbec7fcea7b694e8fcad6b9738e7eebf9b34390
Marco De Canal <marco.decanal@gmail.com>
2020-07-02 12:15:56 +0200

    Terminato consenso informato

--- gestione/views.py
+++ gestione/views.py
@@ -1,4 +1,5 @@
 """Modulo con le views di pazienti e fatture"""
+import datetime
 from django.shortcuts import render
 from django.views import generic
 from .models import Paziente, Fattura
@@ -11,6 +12,20 @@ from reportlab.lib import colors
 from reportlab.lib.units import inch
 from django.contrib.auth.mixins import LoginRequiredMixin
 styles = getSampleStyleSheet()
+mese = {
+    1:'gennaio',
+    2:'febbraio',
+    3:'marzo',
+    4:'aprile',
+    5:'maggio',
+    6:'giugno',
+    7:'luglio',
+    8:'agosto',
+    9:'settembre',
+    10:'ottobre',
+    11:'novembre',
+    12:'dicembre'
+}
 
 # Create your views here.
 #Per limitare l'accesso ai soli utenti registrati nelle Classi, aggiungi come classe
@@ -52,11 +67,11 @@ def home_page(request):
 
 def fattura_pdf(request, pk):
 
-    title_style=ParagraphStyle('title',parent=styles['Normal'],fontName='Helvetica',fontSize=21,spaceAfter=15)    
-    subtitle_style=ParagraphStyle('subtitle',parent=styles['Normal'],fontName='Helvetica',fontSize=18,spaceAfter=25)
-    info_style=ParagraphStyle('info',parent=styles['Normal'],fontName='Helvetica',fontSize=12)
-    heading_style=ParagraphStyle('heading',parent=styles['Normal'],fontName='Helvetica',fontSize=15, spaceAfter=10)
-    bottom_style=ParagraphStyle('bottom',parent=styles['Normal'],fontName='Helvetica',fontSize=10)
+    title_style=ParagraphStyle('title',parent=styles['Normal'],fontName='Helvetica',fontSize=21, leading=25)#,spaceAfter=15)    
+    subtitle_style=ParagraphStyle('subtitle',parent=styles['Normal'],fontName='Helvetica',fontSize=18, leading=23)#,spaceAfter=25)
+    info_style=ParagraphStyle('info',parent=styles['Normal'],fontName='Helvetica',fontSize=12,leading=15)
+    heading_style=ParagraphStyle('heading',parent=styles['Normal'],fontName='Helvetica',fontSize=12, leading=15)#,spaceAfter=10)
+    bottom_style=ParagraphStyle('bottom',parent=styles['Normal'],fontName='Helvetica',fontSize=9,leading=12)
     
     f = Fattura.objects.get(id=pk)
     paz = f.paziente
@@ -64,13 +79,13 @@ def fattura_pdf(request, pk):
 
     buffer = io.BytesIO()
     doc = SimpleDocTemplate(buffer)
-    Story = [Spacer(1, 2*inch)]
+    Story = []#Spacer(1, 0.2*inch)]
     style = styles["Normal"]
 
     p = Paragraph('Marco De Canal', title_style)
     Story.append(p)
     Story.append(Paragraph('Massofisioterapista',subtitle_style))
-    Story.append(Spacer(1, 0.2*inch))
+    Story.append(Spacer(1, 0.3*inch))
 
     text = '''
         <i>Sede legale:</i> Via Brescia 205B, 47842, S. Giovanni in M.no (RN)<br/>
@@ -88,7 +103,7 @@ def fattura_pdf(request, pk):
     '''
     p = Paragraph(text, info_style)
     Story.append(p)
-    Story.append(Spacer(1, 0.2*inch))
+    Story.append(Spacer(1, 0.6*inch))
 
     text = f"Spett.le {paz.cognome} {paz.nome}"
     if(paz.codfisc):
@@ -100,7 +115,7 @@ def fattura_pdf(request, pk):
 
     p = Paragraph(text, heading_style)
     Story.append(p)
-    Story.append(Spacer(1, 0.2*inch))
+    Story.append(Spacer(1, 0.7*inch))
 
     text = f"Fattura numero {f.numero} del {f.data}"
 
@@ -121,15 +136,15 @@ def fattura_pdf(request, pk):
         ('ALIGN',(1,0),(1,2),'CENTER')
     ]))
     Story.append(t)
-    Story.append(Spacer(1, 0.2*inch))
+    Story.append(Spacer(1, 1*inch))
 
     text = '''
         Operazione senza applicazione dell’Iva ai sensi Legge
-        190 del 23/12/2014 art. 1, commi da 54 a 89.<br/>
+        190 del 23/12/2014 art. 1, commi da 54 a 89.<br/><br/>
         Operazione effettuata ai sensi art. 1, commi da 54 a 89,
         della Legge 190 del 23/12/2014 – Regime forfetario.<br/>
         Il compenso non soggetto a ritenute d’acconto ai sensi della Legge
-        190 del 23/12/2014, art. 1 comma 67.<br/>
+        190 del 23/12/2014, art. 1 comma 67.<br/><br/>
         Vi informiamo che tutti i dati a Voi relativi in nostro possesso verranno inseriti
         in un apposito archivio, ai sensi della L. 196/03. Tale trattamento è finalizzato
         all’adempimento di norme di Legge, di obblighi contabili, fiscali, commerciali
@@ -144,6 +159,115 @@ def fattura_pdf(request, pk):
     doc.build(Story)
     buffer.seek(0)
     return FileResponse(buffer, as_attachment=True, filename=nome_file)
+
+def privacy_pdf(request,pk):
+    """Generazione modulo privacy del paziente pk"""
+    paz = Paziente.objects.get(pk=pk)
+    buffer = io.BytesIO()
+    doc = SimpleDocTemplate(buffer)
+    Story = []
+    style = styles["Normal"]
+    
+
+
+    doc.build(Story)
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='privacy.pdf')
+
+def consenso_pdf(request,pk):
+    """Generazione modulo consenso informato del paziente pk"""
+    paz = Paziente.objects.get(pk=pk)
+    buffer = io.BytesIO()
+    doc = SimpleDocTemplate(buffer)
+    Story = []
+    style = styles["Normal"]
+    
+    title_style=ParagraphStyle('title',parent=styles['Normal'],fontName='Helvetica',fontSize=15, leading=18,alignment=1)
+    body_style=ParagraphStyle('body',parent=styles['Normal'],fontName='Helvetica',fontSize=10,leading=12,spaceBefore=2)
+    heading_style=ParagraphStyle('heading',parent=styles['Normal'],fontName='Helvetica',fontSize=11,leading=13,alignment=1)
+    footer_style=ParagraphStyle('footer',parent=styles['Normal'],fontName='Helvetica',fontSize=11,leading=13,alignment=2)
+
+    text = 'il Salice - studio di massofisioterapia e tecniche osteopatiche'
+    p = Paragraph(text,title_style)
+    Story.append(p)
+    Story.append(Spacer(1, 0.2*inch))
+    
+    text = '<font size="11">MODULO DI CONSENSO INFORMATO AL TRATTAMENTO MASSOFISIOTERAPICO</font>'
+    p = Paragraph(text,title_style)
+    Story.append(p)
+    Story.append(Spacer(1, 0.2*inch))
+    
+    text = f'Io sottoscritto {paz.nome} {paz.cognome}, '
+    if(paz.codfisc):
+        text += f'codice fiscale {paz.codfisc} '
+    if(paz.piva):
+        text += f'partita IVA {paz.piva} '
+    if(paz.data_nascita and paz.paese_nascita and paz.provincia_nascita):
+        text += f'<br/>nato/a a {paz.paese_nascita} ({paz.provincia_nascita}) il {paz.data_nascita} '
+    if(paz.paese and paz.cap and paz.via and paz.provincia and paz.civico):
+        text += f'<br/>residente a {paz.paese} ({paz.provincia}), {paz.cap}, in {paz.via} {paz.civico} '
+    p = Paragraph(text,body_style)
+    
+    Story.append(p)
+    Story.append(Spacer(1, 0.2*inch))
+
+    Story.append(Paragraph('DICHIARO',heading_style))
+    Story.append(Spacer(1,0.2*inch))
+
+    Story.append(Paragraph('''di essere stato/a informato/a in modo
+    chiaro ed esauriente da Marco De Canal massofisioterapista
+    ''',body_style))
+    #Story.append(Spacer(1,0.2*inch))
+
+    text = 'sul tipo di trattamento proposto: tecniche, materiali e mezzi utilizzati;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'sulle possibili conseguenze derivanti dalla mancata esecuzione del trattamento suddetto;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'sui benefici attesi, i rischi presunti e le eventuali complicanze ed effetti collaterali;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'sui comportamenti da mettere in atto onde evitare e/o limitare eventuali complicanze durante e dopo il trattamento;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'sulla possibilità di interrompere liberamente ed in qualsiasi momento il trattamento;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'sull’impossibilità a procedere nel trattamento in caso di mancata sottoscrizione del presente consenso.'
+    Story.append(Paragraph(text,body_style,'-'))
+    Story.append(Spacer(1,0.2*inch))
+
+    Story.append(Paragraph('DICHIARO INOLTRE DI',heading_style))
+    Story.append(Spacer(1,0.2*inch))
+
+    text = 'aver pienamente compreso quanto mi è stato verbalmente detto relativamente al trattamento proposto;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'aver avuto l’opportunità di porre domande chiarificatrici e di aver avuto risposte soddisfacenti;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'essere stato/a informato/a dei motivi che consigliano il trattamento proposto e sulla qualità della mia vita in caso di rifiuto;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'aver avuto il tempo sufficiente per decidere;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'essere consapevole che la decisione di accettare il trattamento proposto è volontaria e che posso ritirare il consenso in qualsiasi momento;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'essere stato/a informato/a che tutti i miei dati personali e di salute saranno trattati ai sensi del Regolamento UE 2016/679 relativo alla protezione delle persone fisiche con riguardo al trattamento di dati personali;'
+    Story.append(Paragraph(text,body_style,'-'))
+    text = 'essere stato/a informato/a che per ogni problema o eventuali ulteriori informazioni dovrò rivolgermi a Marco De Canal'
+    Story.append(Paragraph(text,body_style,'-'))
+    Story.append(Spacer(1,0.4*inch))
+
+    Story.append(Paragraph('PERTANTO ACCONSENTO AL TRATTAMENTO',heading_style))
+    Story.append(Spacer(1,0.3*inch))
+
+    data = datetime.date.today()
+    datastr=str(data.day)+" "+mese[data.month]+" "+str(data.year)
+    Story.append(Paragraph(f'San Giovanni in Marignano, {datastr}',body_style))
+    Story.append(Paragraph(f'{paz.nome} {paz.cognome}',body_style))
+    Story.append(Paragraph('Firma: _______________________________________',body_style))
+    Story.append(Spacer(1,0.4*inch))
+
+    Story.append(Paragraph('De Canal Marco<br/>P.Iva 04370000400<br/>Via Brescia 205/B, San Giovanni in Marignano (RN)',footer_style))
+
+    doc.build(Story)
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='consenso informato.pdf')
+
 
 
 # da definire, la View per l'inserimento di un nuovo paziente
