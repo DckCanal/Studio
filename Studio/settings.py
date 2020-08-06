55eeaba26cf4a910d5779382b4e606623330464e
Marco De Canal <marco.decanal@gmail.com>
2020-08-06 15:21:14 +0200

    Deleted DATABASE_URL='postgres://$(marco)' from .env and set local connection to postgresql

--- Studio/settings.py
+++ Studio/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'studio',
         'USER': 'marco',
+        'PASSWORD': 'rfensemble',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
