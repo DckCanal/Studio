c607d60a8216cae5886263a45890a84a8c082d27
Marco De Canal <marco.decanal@gmail.com>
2020-10-31 11:32:13 +0100

    SECURE_HSTS_SECONDS to 60 sec

--- Studio/settings.py
+++ Studio/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['ilsalice.herokuapp.com','127.0.0.1','0.0.0.0']
 
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT','') != 'False'
+SECURE_HSTS_SECONDS = 60
 
 
 # Application definition
