73063850359a71be6fad80e1e18168cc1949e137
Marco De Canal <marco.decanal@gmail.com>
2021-01-29 17:50:59 +0100

    global date_format and language_code

--- Studio/settings.py
+++ Studio/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it-it'
 
 TIME_ZONE = 'Europe/Rome'
 
@@ -136,7 +136,8 @@ USE_I18N = True
 
 USE_L10N = False
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
-DATETIME_FORMAT = 'j/m/Y G:i'
+DATETIME_FORMAT = 'j b Y G:i'
+DATE_FORMAT = 'j b Y'
 USE_TZ = True
 
 
